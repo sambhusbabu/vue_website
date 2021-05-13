c927a6b9ddfe778b0b62151de8bcfce7b46bd888
Sambhu <sambhu.hai96@gmail.com>
2021-05-13 18:16:14 +0530

    -- init

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import store from './store'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-// import 'vue-material/dist/theme/default.css'
-import 'vue-material/dist/theme/default-dark.css'
+import 'vue-material/dist/theme/default.css'
+// import 'vue-material/dist/theme/default-dark.css'
 
 Vue.use(VueMaterial)
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: '/about',
@@ -16,12 +19,26 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: 'About'
+    }
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
+})
+
+// navigation guard to check for logged in users
+router.beforeEach((to, _from, next) => {
+  const DEFAULT_TITLE = 'Web App'
+  next()
+  Vue.nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE
+  })
 })
 
 export default router
